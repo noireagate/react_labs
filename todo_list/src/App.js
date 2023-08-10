cde9d88f4bc755df63750c0881025296b13456f3
noireagate <108992823+noireagate@users.noreply.github.com>
2023-08-10 09:57:26 +0200

    Update App.js

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -5,7 +5,24 @@ const App = () => {
   const [todo, setTodo] = React.useState("");
   
   // Add the handlesubmit code here
-  
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newTodo.text.length > 0 ) {
+        setTodos([...todos].concat(newTodo));
+        setTodo("");
+
+    } else {
+
+        alert("Enter Valid Task");
+        setTodo("");
+    }
+  }
   
   // Add the deleteToDo code here
 
@@ -19,10 +36,16 @@ const App = () => {
 return(
 <div className ="App">
 <h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Add a new task"
+          value={todo}
+        />
+            <button type="submit">Add Todo</button>
+        </form>
+        {todos.map((todo) => <div>{todo.text}</div>)}
 </div>
 );
 };
